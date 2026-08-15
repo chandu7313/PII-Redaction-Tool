007ab29d7aa7acf3260aa161cf051260c6e9f696
chandu7313 <mrcm7313@gmail.com>
2026-08-15 10:59:31 +0530

    Fix uv package metadata name issue by providing pre-built wheel

--- fake_phonenumbers/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="phonenumbers",
-    version="8.99.9",
-    description="Dummy package to satisfy presidio-analyzer phonenumbers dependency. We use phonenumberslite instead.",
-    packages=find_packages(),
-)
